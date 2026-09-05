e2885b74ce63aac72e1d6b9ace1c7531a6a86b13
Yacho-glitch <yahyabousksou@gmail.com>
2026-09-05 01:18:06 +0100

    feat: create task controller

--- task-api/routes/web.php
+++ task-api/routes/web.php
@@ -9,4 +9,5 @@ Route::get('/', function () {
 
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
-Route::put('');
+Route::put('/tasks/{id}', [TaskController::class, 'update']);
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
