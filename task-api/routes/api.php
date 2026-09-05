d4158485207f649fc8d8369140518af115b3c631
Yacho-glitch <yahyabousksou@gmail.com>
2026-09-05 02:52:52 +0100

    feat: install sanctum and use it in api.php

--- task-api/routes/api.php
+++ task-api/routes/api.php
@@ -4,8 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::get("/tasks", [TaskController::class, "index"]);
-Route::post("/tasks", [TaskController::class, "store"]);
-Route::put("/tasks/{id}", [TaskController::class, "update"]);
-Route::delete("/tasks/{id}", [TaskController::class, "destroy"]);
-
+Route::middleware("auth:sanctum")->group(function () {
+    Route::get("/tasks", [TaskController::class, "index"]);
+    Route::post("/tasks", [TaskController::class, "store"]);
+    Route::put("/tasks/{id}", [TaskController::class, "update"]);
+    Route::delete("/tasks/{id}", [TaskController::class, "destroy"]);
+});
