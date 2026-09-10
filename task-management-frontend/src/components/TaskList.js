83e18057ab7d14f0c611a089285f0fdf0b4742b9
Yacho-glitch <yahyabousksou@gmail.com>
2026-09-10 20:33:57 +0100

    feat: complete both of tasklist & navbar page

--- task-management-frontend/src/components/TaskList.js
+++ task-management-frontend/src/components/TaskList.js
@@ -1,25 +1,19 @@
-import { useState, useEffect } from "react";
-import api from "../api/api";
+import React from "react";
+import TaskItem from "./TaskItem";
 
-export default function TaskList() {
-    const [tasks, setTasks] = useState([]);
-
-    useEffect(() => {
-        api.get('/tasks')
-            .then(res => setTasks(res.data.tasks))
-            .catch(err => console.error(err))
-    }, []);
-
+function TaskList({ tasks, onTaskUpdated, onTaskDelete }) {
     return (
-        <div>
-            <h2>My Tasks</h2>
-            <ul>
-                {tasks.map(task => (
-                    <li key={task.id}>
-                        {task.title} - {task.status ? "✅ Done" : "⏳ Pending"}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+        <ul>
+            {tasks.map(task => (
+                <TaskItem 
+                    key={task.id}
+                    task={task}
+                    onTaskUpdated={onTaskUpdated}
+                    onTaskDeleted={onTaskDelete}
+                />
+            ))}
+        </ul>
+    )
 }
+
+export default TaskList;
